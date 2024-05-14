7ece5d840570f8e27f2aff26bdffa83c69d40f55
viktshevchenk <viktoriashevchenk@yandex.ru>
2024-05-14 21:49:42 +0300

    unfinished code

--- user interaction/main.py
+++ user interaction/main.py
@@ -6,51 +6,83 @@ topic2 = "Постоение СКНФ и СДНФ"
 topic3 = "Построение полинома Жегалкина"
 topics = [topic1, topic2, topic3]
 
+groups = ["ИТПМ-123", "ИТПМ-122", "ИТПМ-121", "ИТПМ-120"]
+
 window = Tk()
-window.geometry('400x270')
+window.geometry('405x270')
 window.title('Генератор заданий контрольных работ')
 window.resizable(0, 0)
 window.wm_attributes('-topmost', 1)
 
+group = StringVar(value="ИТПМ-123")
+name = StringVar(value =" ")
+t1 = IntVar(value=1)
+t2 = IntVar(value=1)
+t3 = IntVar(value=1)
 
 def validate(new_value):
     return new_value == "" or new_value.isnumeric()
 
+vcmd = (window.register(validate), '%P')
 
-def CreateFail(group, topic, options, tasks):
-    if topic == topic1:
-        t = 1
-    elif topic == topic2:
-        t = 2
-    elif topic == topic3:
-        t = 3
-    else:
-        t = 0
+def CreateFail(name, group, t1, t2, t3):
     file = open("info.txt", 'w')
-    file.write(f"{group} {t} {options} {tasks}")
+    if name == " ": name = "Преподаватель"
+    file.write(f"{subject} {name} {group} {t1} {t2} {t3}")
     file.close()
     window.destroy()
 
+def clear():
+    all_widgets = window.place_slaves()
+    for l in all_widgets:
+        l.destroy()
 
-group = StringVar()
-topic = StringVar(value=topics[0])
-options = IntVar(value=1)
-tasks = IntVar(value=3)
 
-Label(window, text="Дискретная математика").place(x=140, y=10)
-Label(window, text="Группа: ").place(x=30, y=50)
-ttk.Label(window, text="Выберите тему: ").place(x=30, y=90)
-Label(window, text="Количество вариантов").place(x=30, y=130)
-Label(window, text="Количество заданий в варианте").place(x=30, y=170)
+def subject():
+    b_1 = Button(text='Дискретная математика', font=('Arial', 18), fg='black', bg="DarkSeaGreen1", command=user)
+    b_1.place(x=55, y=75, width=300)
 
-vcmd = (window.register(validate), '%P')
+    b_2 = Button(text='Математический анализ', font=('Arial', 18), bg="DarkSeaGreen1", fg='black', command=user)
+    b_2.place(x=55, y=140, width=300)
 
-Entry(window, textvariable=group, validate='key').place(x=240, y=50)
-ttk.Combobox(values=topics, state="readonly", textvariable=topic, width=35).place(x=130, y=90)
-Entry(window, textvariable=options, validate='key', validatecommand=vcmd).place(x=240, y=130)
-Entry(window, textvariable=tasks, validate='key', validatecommand=vcmd).place(x=240, y=170)
 
-Button(window, text="Сгенерировать контрольную работу", bg="DarkSeaGreen1",
-       command=lambda: CreateFail(group.get(), topic.get(), options.get(), tasks.get())).place(x=155, y=220)
+def user():
+    b_1 = Button(text='Студент', font=('Arial', 18), fg='black', bg="DarkSeaGreen1", command=student)
+    b_1.place(x=55, y=75, width=300)
 
+    b_2 = Button(text='Преподаватель', font=('Arial', 18), bg="DarkSeaGreen1", fg='black', command=teacher)
+    b_2.place(x=55, y=140, width=300)
+
+
+def student():
+    clear()
+
+    Label(window, text="Введите ФИО: ").place(x=30, y=50)
+    Entry(window, textvariable=name, validate='key', width=35).place(x=160, y=50)
+
+    common()
+
+def teacher():
+    clear()
+
+    ttk.Label(window, text="Группа: ").place(x=30, y=50)
+    ttk.Combobox(values=groups, textvariable=group, width=35).place(x=140, y=50)
+
+    common()
+
+def common():
+    Label(window, text=topic1).place(x=30, y=90)
+    Entry(window, textvariable=t1, validate='key', validatecommand=vcmd).place(x=250, y=90)
+
+    Label(window, text=topic2).place(x=30, y=130)
+    Entry(window, textvariable=t2, validate='key', validatecommand=vcmd).place(x=250, y=130)
+
+    Label(window, text=topic3).place(x=30, y=170)
+    Entry(window, textvariable=t3, validate='key', validatecommand=vcmd).place(x=250, y=170)
+
+    Button(window, text="Сгенерировать работу", bg="DarkSeaGreen1",
+           command=lambda: CreateFail(name.get(), group.get(), t1.get(), t2.get(), t3.get())).place(x=155, y=220)
+
+
+subject()
 window.mainloop()
