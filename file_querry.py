e630ce40878bf5d22df04e72f0cd485fdd1ca5be
Ang3l0v <148868320+Ang3l0v@users.noreply.github.com>
2024-05-23 07:54:21 +0300

    Add files via upload

--- file_querry.py
+++ file_querry.py
@@ -3,8 +3,19 @@ import traceback
 import random
 import pylatex
 import chardet
+from reportlab.pdfgen import canvas  # pip install reportlab
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase import pdfmetrics
 from datetime import datetime
 from funcs_for_file_querry import *
+from datetime import date
+
+center = 230
+start = 100
+top = 790
+interval = 22
+
+pdfmetrics.registerFont(TTFont('Arial', 'Arial.ttf'))  # Загружаем шрифт с поддержкой кириллицы
 try:
     
     conn = psycopg2.connect(dbname='practice', user='postgres', password='12345', host='localhost')
@@ -32,8 +43,10 @@ try:
 
             amount_tasks = int(bool_f[0]) + int(sknf_sdnf_f[0]) + int(zegalkin_f[0])  # кол-во заданий в варианте
         else:
-            subject = info_file[0] + info_file[1]
+            subject = info_file[0] + ' '+ info_file[1]
             name = info_file[2] + ' ' + info_file[3] + ' ' + info_file[4]
+
+            student_name = name
 
             bool_f = [info_file[5], info_file[6]] 
             sknf_sdnf_f = [info_file[7], info_file[8]]
@@ -131,6 +144,9 @@ try:
 
         variant = ','.join(map(str, variant))
         answers = ','.join(map(str, answers))
+        variant_list = variant.split(',')
+        answers_list = answers.split(',')
+
 
         with conn.cursor() as cursor:
             querry = "insert into variants (id_template, text, answers, num) values (%s, %s, %s, %s) returning id"
@@ -149,14 +165,71 @@ try:
                 querry3 = "insert into tasks (id_stud_var, text_task, text_ans) values (%s, %s, %s)"
                 cursor.execute(querry3, (id_stud_var, tasks_and_answers[i], tasks_and_answers[i+1]))
                 i += 2
+        k3 = 1
+        doc_for_one_student = canvas.Canvas(student_name + ' вариант.pdf')
+        pdfmetrics.registerFont(TTFont('Arial', 'Arial.ttf'))  # Загружаем шрифт с поддержкой кириллицы
+        doc_for_one_student.setFont("Arial", 12)  # Устанавливаем текущий шрифт
+
+        doc_for_one_student.drawString(500, 790, date.today().strftime("%d.%m.%Y"))  # Добавляем дату
+        k3 += 1
+        doc_for_one_student.drawString(center, 790, subject)  # Добавляем название предмета
+        k3 += 1
 
 
 
+        for i in variant_list:
+            doc_for_one_student.drawString(start, top - interval * k3, i)
+            k3 += 1
+        doc_for_one_student.save()
+
+
+
+
+
+        k4 = 1
+        doc_for_one_student_answers = canvas.Canvas(student_name + ' ответы.pdf')
+        pdfmetrics.registerFont(TTFont('Arial', 'Arial.ttf'))  # Загружаем шрифт с поддержкой кириллицы
+        doc_for_one_student_answers.setFont("Arial", 12)  # Устанавливаем текущий шрифт
+        doc_for_one_student_answers.drawString(500, 790, date.today().strftime("%d.%m.%Y"))  # Добавляем дату
+        k4 += 1
+        doc_for_one_student_answers.drawString(center, 790, subject)  # Добавляем название предмета
+        k4 += 1
+
+
+
+
+        for j in answers_list:
+            doc_for_one_student_answers.drawString(start, top - interval * k4, j)
+            k4 += 1
+        doc_for_one_student_answers.save()
+
+
+
+    #для всей группы
 
 
     else:
+        answers_all_group = []
 
 
+        k2 = 1
+        doc_for_teacher = canvas.Canvas("Ответы.pdf")  # Создаем новый PDF файл
+
+        pdfmetrics.registerFont(TTFont('Arial', 'Arial.ttf'))  # Загружаем шрифт с поддержкой кириллицы
+
+        doc_for_teacher.setFont("Arial", 12)  # Устанавливаем текущий шрифт
+
+        doc_for_teacher.drawString(500, 790, date.today().strftime("%d.%m.%Y"))  # Добавляем дату
+
+        doc_for_teacher.drawString(center, 790, subject)  # Добавляем название предмета
+        k2 += 1
+
+
+
+
+
+
+        number_variant = 1
         with conn.cursor() as cursor:
 
             querry = "select id from students where id_group = (select id from band where name = %s)"
@@ -167,6 +240,11 @@ try:
         id_student_list_index = 0
 
         for student in range(num_stundents):
+
+
+
+
+
 
             variant = []
             templates_list = []
@@ -185,10 +263,12 @@ try:
 
 
 
+
             first_inter_bool_f = True
             for i in range(int(bool_f[0])):
                 if first_inter_bool_f == True:
-                    #variant.append('Способы задания булевых функций')
+                    variant.append('Способы задания булевых функций')
+                    answers.append('Способы задания булевых функций')
 
                     with conn.cursor() as cursor:
                         querry = "select id from templates where name = %s"
@@ -223,7 +303,10 @@ try:
             first_inter_zegalkin_f = True
             for i in range(int(zegalkin_f[0])):
                 if first_inter_zegalkin_f == True:
-                    #variant.append('Построение полинома Жегалкина')
+
+                    variant.append('Построение полинома Жегалкина')
+                    answers.append('Построение полинома Жегалкина')
+
 
                     with conn.cursor() as cursor:
                         querry = "select id from templates where name = %s"
@@ -257,7 +340,8 @@ try:
             for i in range(int(sknf_sdnf_f[0])):
                 if first_inter_sknf_sdnf_f == True:
 
-                    #variant.append('Построение СКНФ и Построение СДНФ')
+                    variant.append('Построение СКНФ и Построение СДНФ')
+                    answers.append('Построение СКНФ и Построение СДНФ')
                     with conn.cursor() as cursor:
                         querry = "select id from templates where name = %s"
                         name = 'Построение СКНФ'
@@ -310,6 +394,56 @@ try:
 
             variant = ','.join(map(str, variant))
             answers = ','.join(map(str, answers))
+            variant_list = variant.split(',')
+            answers_list = answers.split(',')
+            #print(variant_list)
+            #print(answers_list)
+
+            k = 1
+
+            doc_for_student = canvas.Canvas(student_name[0] + ".pdf")
+            doc_for_student.setFont("Arial", 12)
+            doc_for_student.drawString(500, top, date.today().strftime("%d.%m.%Y"))
+            doc_for_student.drawString(center, top, subject)
+            doc_for_student.drawString(center, top - interval *  k, "Вариант " + str(number_variant))
+            k += 1
+            doc_for_student.drawString(start, top - interval  * k, "Задания:")
+            k += 1
+
+
+            k2 += 1
+
+            line_count = 1
+
+
+
+           # first_iter_pdf = True
+            for i in variant_list:
+
+                doc_for_student.drawString(start, top - interval * k, i)
+                k += 1
+
+
+            for j in answers_list:
+
+
+                #doc_for_teacher.drawString(start, top - interval)
+                doc_for_teacher.drawString(start, top - interval * k2, j)
+                k2 += 1
+                if k2 > 28:
+                    doc_for_teacher.showPage()
+                    doc_for_teacher.setFont("Arial", 12)  # Устанавливаем текущий шрифт
+                    k2 = 1
+
+
+            number_variant += 1
+            doc_for_student.save()
+
+
+
+
+
+
 
             with conn.cursor() as cursor:
 
@@ -344,6 +478,7 @@ try:
 
 
             id_student_list_index += 1
+        doc_for_teacher.save()
 
 
 
